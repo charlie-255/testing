d29b34def1ae44b006bd0ef4bdae10ff7be15f6e
vasudevan <vasudevan@ggf.vom>
2021-01-09 16:12:45 +0530

    first commit

---
+++ function (1).js
@@ -0,0 +1,78 @@
+function colory(){
+    var input = document.getElementById('name');
+    input.style.backgroundColor = 'yellow'
+}
+function colorv(){
+    var input = document.getElementById('name');
+    input.style.backgroundColor = 'white'
+}
+
+
+
+
+// function colorchanger(){
+// var input = document.getElementById('name').value;
+// if(input.length < 10){
+//     console.log(document.getElementById('name'))
+//     document.getElementById('name').style.border = '5px solid red';
+// }
+// else{
+//     document.getElementById('name').style.border = '5px solid green';
+// }
+// }
+
+// function imgtogle(){
+//     console.log(document.getElementById('imgt').src);
+//     if(document.getElementById('imgt').src =='file:///C:/Users/VASUDEVAN/Desktop/jstuto/off.jfif'){
+//         document.getElementById('imgt').src= 'download.jfif';
+//     }    
+//     else{
+//         document.getElementById('imgt').src= 'off.jfif';
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function call(){
+//     // document.getElementById("stu").innerHTML  = '<h1> Gowtham called me</h1>';
+//     // var a = 0;
+//     // while(a<9){
+//     //     document.getElementsByTagName('p')[a].innerHTML = '<h1> Gowtham called me</h1>';
+//     //     a++;
+//     // }
+//     var a = document.querySelectorAll('div #stu')[1].innerHTML = '<h1> hi </h1>';
+// }
+
+// // // var a = document.getElementById('person').innerHTML;
+// // // var b = document.getElementById('person').innerHTML = a + ' Gowtham';
+// // // document.getElementById('person').className = 'newclass2';
+// // // console.log(b);
+
+// // function tuto(){
+// //     var img = document.getElementsByTagName('img')[0];
+// //     img.src = 'off.jfif';
+// //     img.alt = 'off image';
+// //     console.log(img);
+// // }
+// // const a = 0;
+
+// // console.log(a);
+
+// // // let a = document.getElementById('stu').style.backgroundColor = 'red';
+// // // console.log(a);
---
+++ function.js
@@ -0,0 +1,20 @@
+// var a = document.getElementById('person').innerHTML;
+// var b = document.getElementById('person').innerHTML = a + ' Gowtham';
+// document.getElementById('person').className = 'newclass2';
+// console.log(b);
+
+function tuto(){
+    var img = document.getElementsByTagName('img')[0];
+    img.src = 'off.jfif';
+    img.alt = 'off image';
+    console.log(img);
+}
+const a = 0;
+//my first git hub
+//testing
+//gfulhdui
+
+console.log(a);
+
+// let a = document.getElementById('stu').style.backgroundColor = 'red';
+// console.log(a);
